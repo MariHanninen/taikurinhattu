251cbe047504df479be9700541332c613ae13f3a
Mari Hänninen <43204068+MariHanninen@users.noreply.github.com>
2018-09-22 11:45:12 +0300

    Add files via upload

---
+++ numeroleikki.py
@@ -0,0 +1,25 @@
+from random import randint
+import sys
+
+
+def arvaa_numero():
+    numero = randint(0, 100)
+    while True:
+
+        syote = int(input("Arvaa joku numero 0-100 väliltä: "))
+        if syote < 0:
+            print("Huono valinta!")
+            print("Lopetetaan.")
+            sys.exit(0)
+        if syote == numero:
+            message = "\nArvasit oikein numero on {}!"
+            print(message.format(numero))
+            break
+        elif syote < numero:
+            print("Numero on suurempi.")
+        else:
+            print("Numero on pienempi.")
+
+
+if __name__ == "__main__":
+    arvaa_numero()
